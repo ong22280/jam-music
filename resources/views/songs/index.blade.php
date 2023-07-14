f990e652b901af48e64bac0fc1e723d80f42dc34
ong22280 <ong22280@ong-Inspiron15>
2023-07-14 20:52:22 +0700

    add: Layout

--- resources/views/songs/index.blade.php
+++ resources/views/songs/index.blade.php
@@ -1,35 +1,21 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CS442 - Songs</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-<body>
-    <nav class="flex">
-        <a class="ml-2" href="{{ route('about.index') }}">About</a>
-        <a class="ml-2" href="{{ route('songs.index') }}">Songs</a>
-    </nav>
+@extends('layouts.main')
 
-    <div class="bg-white shadow-md rounded-md overflow-hidden max-w-lg mx-auto mt-16">
-        <div class="bg-pink-100 py-2 px-4">
+@section('content')
+    <div class="max-w-lg mx-auto mt-16 overflow-hidden bg-white rounded-md shadow-md">
+        <div class="px-4 py-2 bg-pink-100">
             <h2 class="text-xl font-semibold text-gray-800">{{ $title }}</h2>
         </div>
         <ul class="divide-y divide-gray-200">
             @foreach ($songs as $song)
-            <li class="flex items-center py-4 px-6 hover:bg-gray-50">
-                <span class="text-gray-700 text-lg font-medium mr-4">{{ $loop->iteration }}.</span>
+            <li class="flex items-center px-6 py-4 hover:bg-gray-50">
+                <span class="mr-4 text-lg font-medium text-gray-700">{{ $loop->iteration }}.</span>
                 <div class="flex-1">
                     <h3 class="text-lg font-medium text-gray-800">{{ $song['title'] }}</h3>
-                    <p class="text-gray-600 text-base">{{ $song['artist'] }}</p>
+                    <p class="text-base text-gray-600">{{ $song['artist'] }}</p>
                 </div>
                 <span class="text-gray-400">{{ $song['duration']['minutes'] }}:{{ $song['duration']['seconds'] }}</span>
             </li>
             @endforeach
         </ul>
     </div>
-</body>
-</html>
+@endsection
